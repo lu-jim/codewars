4fa984c5e7ca36be877535c5980e8481daf9d614
Luis Fernando Jimenez <lu.jim@protonmail.com>
2022-05-26 23:59:35 -0400

    Solve Valid Anagram problem

--- leetcode/242-validAnagram.js
+++ leetcode/242-validAnagram.js
@@ -1,3 +1,6 @@
 const validAnagram = (string_1, string_2) => {
-  
+  if (s.length !== t.length) return false
+    sWord = s.split('').sort().join('')
+    tWord = t.split('').sort().join('')
+    return sWord === tWord
 };
